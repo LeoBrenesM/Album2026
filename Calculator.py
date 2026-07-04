b853463f3773e076bb0cfda3a13391cacdf8122b
Leonardo <lbrenes1098@gmail.com>
2026-07-04 09:10:57 -0600

    Update Calculator.py
    modified calculator, new prices  FWC 03-08 and 17-18 are now at 2000
    
    moved subtotal column to the end
    
    and only rows with +1 sticker will appear.

--- Calculator.py
+++ Calculator.py
@@ -10,10 +10,10 @@ def show_calculator():
     equipos = st.number_input("Equipos", 0, 999, 0)
     escudos = st.number_input("Escudos", 0, 999, 0)
     fwc00 = st.number_input("FWC00", 0, 999, 0)
-    fwcTier1 = st.number_input("FWC 03, 04, 05, 08 - 17, 18", 0, 999, 0)
+    fwcTier1 = st.number_input("FWC 03-08, 17, 18", 0, 999, 0)
     fwcTier2 = st.number_input("FWC 01,02,04,06,07,08, [09-16],19", 0, 999, 0)
-    tier1 = st.number_input("Lamine, Julián , Raphinha, Vinicius", 0, 999, 0)
-    tier2 = st.number_input("Mbappé / Modric / Haaland / Bellingham", 0, 999, 0)
+    tier1 = st.number_input("Lamine, Julián, Raphinha, Vinicius", 0, 999, 0)
+    tier2 = st.number_input("Mbappé, Modric, Haaland, Bellingham", 0, 999, 0)
     tier3 = st.number_input("Messi / CR7", 0, 999, 0)
 
     total_figuritas = (normales + equipos + escudos + 
@@ -29,7 +29,7 @@ def show_calculator():
         escudos * 500 +
         equipos * 300 +
         fwc00 * 500 +
-        fwcTier1 * 1000 +
+        fwcTier1 * 2000 +
         fwcTier2 * 500 +
         tier1 * 1500 +
         tier2 * 3000 +
@@ -39,22 +39,24 @@ def show_calculator():
     if total > 0:
 
         detalle = pd.DataFrame([
-            ["Normales", normales, 150, normales * 150],
-            ["Equipos", equipos, 300, equipos * 300],
-            ["Escudos", escudos, 500, escudos * 500],
-            ["FWC 00", fwc00, 500, fwc00 * 500],
-            ["FWC 03, 04, 05, 08 - 17, 18", fwcTier1, 1000, fwcTier1 * 1000],
-            ["FWC 01,02,04,06,07,08", fwcTier2, 500, fwcTier2 * 500],
-            ["Lamine, Julián , Raphinha, Vinicius", tier1, 1500, tier1 * 1500],
-            ["Mbappé / Modric / Haaland / Bellingham", tier2, 3000, tier2 * 3000],
-            ["Messi / CR7", tier3, 4000, tier3 * 4000],
+            ["Normales", normales, normales * 150, 150],
+            ["Equipos", equipos, equipos * 300, 300],
+            ["Escudos", escudos, escudos * 500, 500],
+            ["FWC 00", fwc00, fwc00 * 500, 500],
+            ["FWC 01,02", fwcTier2, fwcTier2 * 500, 500],
+            ["FWC 03-08, 17, 18", fwcTier1, fwcTier1 * 2000, 2000],
+            ["Lamine, Julián , Raphinha, Vini", tier1, tier1 * 1500,1500],
+            ["Mbappé, Modric, Haaland, Belling", tier2, tier2 * 3000, 3000],
+            ["Messi / CR7", tier3, tier3 * 4000, 4000],
         ],
         columns=[
             "Tipo",
             "Cantidad",
-            "Precio Unitario",
             "Subtotal"
+            "Precio Unitario",
         ])
+
+        detalle = detalle[detalle["Cantidad"] > 0]
 
         st.dataframe(
             detalle,
@@ -70,6 +72,6 @@ def show_calculator():
             )
 
         st.metric(
-            "Valor total estimado",
+            "Valor total",
             f"₡{total:,}"
         )
